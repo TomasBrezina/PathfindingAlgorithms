a83732ef60c8f24bff7476c4f637d76932b28100
Tomas Brezina <46631861+TomasBrezina@users.noreply.github.com>
2021-06-23 20:59:54 +0200

    Added Depth first search algorithm

--- PathfindingAlgorithms/classes/Tools.cs
+++ PathfindingAlgorithms/classes/Tools.cs
@@ -12,6 +12,7 @@ namespace PathfindingAlgorithms
         {
             "Dijkstra",
             "BFS",
+            "DFS",
             "A Star"
         };
 
@@ -22,6 +23,7 @@ namespace PathfindingAlgorithms
                 case "BFS": return "https://en.wikipedia.org/wiki/Breadth-first_search";
                 case "Dijkstra": return "https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm";
                 case "A Star": return "https://en.wikipedia.org/wiki/A*_search_algorithm";
+                case "DFS": return "https://en.wikipedia.org/wiki/Depth-first_search";
                 default: return null;
             }
         }
@@ -31,6 +33,8 @@ namespace PathfindingAlgorithms
             {
                 case "BFS":
                     return new BFS(env.StartNode, env.EndNode, env.Nodes);
+                case "DFS":
+                    return new DFS(env.StartNode, env.EndNode, env.Nodes);
                 case "Dijkstra":
                     return new Dijkstra(env.StartNode, env.EndNode, env.Nodes);
                 case "A Star":
---
+++ PathfindingAlgorithms/classes/pathfinding_algorithms/DFS.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfindingAlgorithms
+{
+    /*
+    Depth First Search
+    Unweighted
+    O(v+e)
+    */
+    public class DFS : PathfindingAlgorithm
+    {
+        /// <summary>
+        /// Array of predecessors.
+        /// </summary>
+        private Node[] Pred;
+
+        /// <summary>
+        /// Queue of nodes as revealed.
+        /// </summary>
+        private Stack<Node> Stack;
+
+        public DFS(Node startNode, Node endNode, List<Node> graph) : base(startNode, endNode, graph)
+        {
+            // Initialization
+            Stack = new Stack<Node>();
+            Pred = new Node[graph.Count];
+
+            // Add start node to the queue
+            Stack.Push(startNode);
+            startNode.SetState(NodeState.Visited);
+        }
+        public override void Step()
+        {
+            switch (PathExists)
+            {
+                // If dont know if path exists
+                case Exist.Unknown:
+                    SearchStep();
+                    break;
+                // If reached End Node and sure about the path
+                case Exist.True:
+                    PathStep();
+                    break;
+                // If algorithm finished search and didnt find a path
+                case Exist.False:
+                    break;
+            }
+        }
+        // Searching trought graph
+        private void SearchStep()
+        {
+            if (Stack.Count > 0) // if not empty
+            {
+                Node node = Stack.Pop();
+                node.SetState(NodeState.Visited);
+                foreach (Edge edge in node.Edges)
+                {
+                    Node nextNode = edge.Node;
+                    if (nextNode.Type != NodeType.Wall && nextNode.State == NodeState.Unseen)
+                    {
+                        nextNode.SetState(NodeState.Revealed);
+                        Pred[nextNode.ID] = node; // Set predecessor 
+                        Stack.Push(nextNode); // Add node to queue
+                        if (nextNode.Type == NodeType.End)
+                        {
+                            PathExists = Exist.True; // If reached end node
+                            Path.Add(nextNode); // Add end node to path
+                        }
+                    }
+                }
+            }
+            else if (PathExists == Exist.Unknown) PathExists = Exist.False;
+        }
+        // Backtracking for path if reached both start and end node
+        private void PathStep()
+        {
+            if (PathExists == Exist.True)
+            {
+                int id = Path.Last().ID;
+                if (id != StartNode.ID)
+                {
+                    Path.Add(Pred[id]);
+                }
+                else PathFound = true;
+            }
+        }
+    }
+}
